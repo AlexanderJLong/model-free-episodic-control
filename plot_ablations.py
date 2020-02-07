3b04654ccf0d24872aaf67d0bc31901224f8757a
alex-lab <alex.long@unsw.edu.au>
2020-02-07 14:48:22 +1100

    Added results and made ablation plotter better

--- plot_ablations.py
+++ plot_ablations.py
@@ -3,22 +3,24 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-files = [("alpha_processed.csv", "TIME-SIG"), ("ab_time_proc.csv", "TIME-SIG"),
-         ("alpha_processed.csv", "TIME-SIG"), ]
+files = [("alpha_processed.csv", "TIME-SIG", "a)", "\u03B1"), ("ab_time_proc.csv", "TIME-SIG", "b)", "Interpolation"),
+         ("alpha_processed.csv", "TIME-SIG", "c)", "Representation"), ]
 fig, ax = plt.subplots(1, len(files), sharey=True)
 sns.set(rc={'figure.figsize': (5, 4)})
 sns.set_context("notebook")
 sns.set_style("ticks")
 for f, ax in zip(files, ax):
-    ss = pd.read_csv(f"./results/{f[0]}")
-    print(ss)
     compare_var = f[1]
+
+    ss = pd.read_csv(f"./results/{f[0]}")
+    ss = ss[["Step", "ENV", "normalized_reward", compare_var]]
+    print(ss)
     max_frames = 80_000
     num_games = ss["ENV"].nunique()
     ss[compare_var] = ss[compare_var].astype('category')
     num_lines = ss[compare_var].nunique()
 
-    sns.lineplot("Step",
+    g = sns.lineplot("Step",
                  "normalized_reward",
                  ci=0,
                  estimator=np.median,
@@ -26,8 +28,9 @@ for f, ax in zip(files, ax):
                  linewidth=1.5,
                  hue=compare_var,
                  palette=sns.color_palette("colorblind", num_lines),
-                 ax=ax
+                 ax=ax,
                  )
+    ax.get_legend().get_texts()[0].set_text(f[3])
     ax.plot((0, max_frames), (0.161, 0.161), c="k", linewidth=2, ls="--", )
     # label="DE-Rainbow")
     ax.plot((0, max_frames), (0.134, 0.134), c="k", linewidth=2, ls=":", )
@@ -35,6 +38,7 @@ for f, ax in zip(files, ax):
     ax.plot((0, max_frames), (0.212, 0.212), c="k", linewidth=2, ls="-", )
     # label="ARK2")
     ax.set_xlim(0, 8e4)
+    ax.set_title(f[2], fontsize=11)
     ax.set_ylabel("Median Human Normalized Reward")
 
     # Put a legend below current axis
@@ -48,8 +52,18 @@ for f, ax in zip(files, ax):
     #ax.yscale("linear")
     #ax.xlabel("Interactions")
 
+
+from matplotlib.lines import Line2D
+legend_elements = [
+    Line2D([0], [0], color='k', lw=2, ls= "--", label='DE Rainbow'),
+    Line2D([0], [0], color='k', lw=2, ls= ":", label='SimPLe'),
+    Line2D([0], [0], color='k', lw=2, ls= "-", label='AKR2'),
+ ]
+
+plt.figlegend(handles=legend_elements, loc="lower center", ncol=3, bbox_to_anchor=[0.5, 0], borderaxespad=0)
 plt.gcf().set_size_inches(12, 5)
-plt.tight_layout()
-plt.savefig(f"./plots/mhns.png")
-plt.savefig(f"./plots/mhns.pdf", format="pdf")
+plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.17)
+#plt.tight_layout()
+plt.savefig(f"./plots/ablation.png")
+plt.savefig(f"./plots/ablatoin.pdf", format="pdf")
 plt.show()
